aed893c3225f495dea15544be6696e2e66b6b709
Ayush <ayush7hx@users.noreply.github.com>
2026-09-19 00:40:36 +0530

    Restore msg alias for owner DM command

--- cogs/commands/owner.py
+++ cogs/commands/owner.py
@@ -307,7 +307,7 @@ class Owner(commands.Cog):
 
 
 
-    @commands.command()
+    @commands.command(aliases=["msg"])
     @commands.is_owner()
     async def dm(self, ctx, user: discord.User, *, message: str):
         """ DM the user of your choice """
